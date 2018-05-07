4d3f8f959804af8d75b0bd881893f681b6896d3d
JuanAntonioReyes <jantonio.reyes.navarro@gmail.com>
2018-05-07 11:02:46 +0200

    Refactorization started

--- js/main.js
+++ js/main.js
@@ -12,27 +12,16 @@ function decapitalize(string) {
 		return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
-function stringToClipboard(string) {
-	// Create a dummy input to copy the string array inside it
-	var dummy = document.createElement("textarea");
-
-	// Add it to the document
-	document.body.appendChild(dummy);
-
-	// Set its ID
-	dummy.setAttribute("id", "dummy_id");
-
-	// Output the array into it
-	document.getElementById("dummy_id").value = string
+function stringToResult(string) {
+	// Output the array into the result text area
+	var resultTextArea = document.getElementById("resultTextArea");
+	resultTextArea.value = string;
 
 	// Select it
-	dummy.select();
+	resultTextArea.select();
 
 	// Copy its contents
 	document.execCommand("copy");
-
-	// Remove it as its not needed anymore
-	document.body.removeChild(dummy);
 
 	alert("Copied to clipboard!");
 }
@@ -44,20 +33,20 @@ $('#btnProcess').click(buttonHandler);
 function buttonHandler(e) {
 	var textArea = $('#textArea');
 	var lines = textArea.val().split('\n');
-	var names = generateNames(lines);
 	
-	//console.log(names);
+	var components = generateComponents(lines);
 
-	var imports = generateImports(names);
-	var routes = generateRoutes(names);
+	var imports = generateImports(components);
+	var routes = generateRoutes(components);
 
-	stringToClipboard(imports + "\n" + routes);
+	stringToResult(imports + "\n" + routes);
 }
 	
-function generateNames(lines) {
-	var lineWords, names = [];
+function generateComponents(lines) {
+	var lineWords, components = [];
 
 	for (var i = 0, l = lines.length; i < l; i++) {
+		// Separate each line into words
 		lineWords = lines[i].trim().split(' ');
 
 		var startIndex;
@@ -73,25 +62,25 @@ function generateNames(lines) {
 		}
 
 		if (lineWords[0] === '-') {
-			names[names.length - 1].children.push(nameText);
+			components[components.length - 1].children.push(nameText);
 		} else {
-			var name = {
+			var newComponent = {
 				text: nameText,
 				children: []
 			};
 
-			names.push(name);
+			components.push(newComponent);
 		}
 	}
 
-	return names;
+	return components;
 }
 
-function generateImports(names) {
+function generateImports(components) {
 	var importLine, imports = '';
 
-	for (var i = 0, l = names.length; i < l; i++) {
-		importLine = "import " + names[i].text + " from './components/" + names[i].text + ".vue';\n";
+	for (var i = 0, l = components.length; i < l; i++) {
+		importLine = "import " + components[i].text + " from './components/" + components[i].text + ".vue';\n";
 	
 		imports += importLine;
 	}
@@ -99,25 +88,25 @@ function generateImports(names) {
 	return imports;
 }
 
-function generateRoutes(names) {
+function generateRoutes(components) {
 	var routes = "export const routes = [\n";
 	var decapName;
 
-	for (var i = 0, l = names.length; i < l; i++) {
-		decapName = decapitalize(names[i].text);
+	for (var i = 0, l = components.length; i < l; i++) {
+		decapName = decapitalize(components[i].text);
 
-		routes += "\t{ path: '/" + decapName + "' , component: " + names[i].text 
+		routes += "\t{ path: '/" + decapName + "' , component: " + components[i].text 
 							+ " , name: '" + decapName + "Link'";
 	
-		if (names[i].children.length !== 0) {
+		if (components[i].children.length !== 0) {
 			routes += ",\n\t\tchildren: [\n";
 
 			var decapChild;
-			for (var j = 0, lc = names[i].children.length; j < lc; j++) {
-				decapChild = decapitalize(names[i].children[j]);
+			for (var j = 0, lc = components[i].children.length; j < lc; j++) {
+				decapChild = decapitalize(components[i].children[j]);
 
 				routes += "\t\t\t{ path: '" + decapChild + "' , component: "
-									+ names[i].children[j] + " , name: '" + decapChild
+									+ components[i].children[j] + " , name: '" + decapChild
 									+ "Link' }";
 			
 				if (j < (lc - 1)) {
